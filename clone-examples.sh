50b68eca88113f65feaa0602fbca24545fde0af9
Joshua Copeland <joshuarcopeland@gmail.com>
2018-10-14 11:19:03 -0700

    updated setup scripts to give more feedback on next steps

--- clone-examples.sh
+++ clone-examples.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo "Cloning AWS initialization and Jenkins setup Terraform Recipes"
 git clone git@github.com:OGProgrammer/terraform-aws-init.git
 git clone git@github.com:OGProgrammer/terraform-aws-jenkins.git
+echo "2 Repos cloned successfully"
+echo "Now head to https://github.com/OGProgrammer/terraform-example-manifest and fork or copy that repo. Clone that forked copy down as yours to edit."
